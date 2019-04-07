5d6e2be0127faf73f68c1f2cc0ddfe75d3f0898e
Chadjiminas Chrysostomos <chadjiminas.chrysostomos@gmail.com>
2019-04-07 17:07:16 +0300

    init commit

---
+++ testprocess/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace testprocess
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.Out.WriteLine("This is written in stdout");
+            Console.Out.WriteLine($"Arguments:\n [{String.Join(", ",args)}]");
+
+            Console.Error.WriteLine("This is written in stderr");
+            
+            //Console.ReadLine();
+            return 0;
+        }
+    }
+}
